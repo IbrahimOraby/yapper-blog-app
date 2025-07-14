cb60df6d453b4c5d7abe822aa3f7a526f5e50632
IbrahimOraby <orabyibrahim19@gmail.com>
2025-07-14 19:25:33 +0300

    feat: add routing, init two routes home and abou

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { Link, Outlet } from "react-router";
+
 function App() {
-	return <>Hello Yapper</>;
+	return (
+		<>
+			<header>
+				<nav className="">
+					<ul>
+						<li className="nav">
+							<Link className="btn" to={"about"}>
+								About
+							</Link>
+						</li>
+						<li>
+							<Link className="btn" to={""}>
+								Home
+							</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<Outlet />
+			<footer>
+				<div>Footer</div>
+			</footer>
+		</>
+	);
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,28 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import "./index.css";
 import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <App />,
+		children: [
+			{
+				path: "/",
+				element: <Home />
+			},
+			{
+				path: "/about",
+				element: <About />
+			}
+		]
+	}
+]);
+
+createRoot(document.getElementById("root")).render(
+	<RouterProvider router={router} />
+);
