aebbf02551043cd2ada701bf75558460c2a487c4
IbrahimOraby <orabyibrahim19@gmail.com>
2025-07-14 20:24:44 +0300

    feat: create layout & theme
    - created a Header & Footer components
    - added 'nord' theme from 'daisyui'

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import { Link, Outlet } from "react-router";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import React from "react";
 
 function App() {
 	return (
 		<>
-			<header>
-				<nav className="">
-					<ul>
-						<li className="nav">
-							<Link className="btn" to={"about"}>
-								About
-							</Link>
-						</li>
-						<li>
-							<Link className="btn" to={""}>
-								Home
-							</Link>
-						</li>
-					</ul>
-				</nav>
-			</header>
-			<Outlet />
-			<footer>
-				<div>Footer</div>
-			</footer>
+			<Header />
+			<main className="flex-1">
+				<Outlet />
+			</main>
+			<Footer />
 		</>
 	);
 }
