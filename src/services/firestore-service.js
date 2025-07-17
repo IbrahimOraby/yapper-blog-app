19d6ad8573f5b56b24b41e6497fff976b5aae1de
IbrahimOraby <orabyibrahim19@gmail.com>
2025-07-17 23:26:29 +0300

    feat: add createPost in firestore with basic form for adding posts

--- src/services/firestore-service.js
+++ src/services/firestore-service.js
@@ -1,4 +1,4 @@
-import { doc, getFirestore, serverTimestamp, setDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getFirestore, serverTimestamp, setDoc } from "firebase/firestore";
 import { app } from "../../firebaseConfig";
 
 // Initialize Cloud Firestore and get a reference to the service
@@ -14,5 +14,19 @@ export const createUser = async (userData) => {
 		console.log("User created successfully");
 	} catch (error) {
 		console.error("Error creating user: ", error);
+	}
+};
+
+export const createPost = async (userData, content) => {
+	try {
+		await addDoc(collection(db, "posts"), {
+			uid: userData.uid,
+			userName: userData.displayName,
+			createdAt: serverTimestamp(),
+			postContent: content
+		});
+		console.log("Post created successfully with uid:", userData.uid);
+	} catch (error) {
+		console.error("Error creating post", error);
 	}
 };
